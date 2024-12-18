308f3ec979ea1cd18e87353e648fb35ba2bc79ae
Dan7Te <danilatelpis3@gmail.com>
2024-12-18 21:59:11 +0300

    Добавьте файлы проекта.

---
+++ UP_02/Pages/AddPage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UP_02
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPage.xaml
+    /// </summary>
+    public partial class AddPage : Page
+    {
+        private Partners _currentPartners = new Partners();
+        public AddPage(Partners selectedPartners)
+        {
+            InitializeComponent();
+            Type.ItemsSource = Entities1.GetContext().PartnersType.ToList();
+            if (selectedPartners != null)
+                _currentPartners = selectedPartners;
+
+            DataContext = _currentPartners;
+        }
+
+        private void Tel_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Разрешаем только цифры
+            e.Handled = !Regex.IsMatch(e.Text, @"^\d$");
+        }
+
+   
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            // Регулярное выражение для проверки ИНН
+            string innPattern = @"^\d{10}$";
+
+            // Проверка поля INN
+            if (!Regex.IsMatch(INN.Text, innPattern))
+            {
+                errors.AppendLine("ИНН должен содержать ровно 10 цифр и не должен содержать букв.");
+            }
+
+            if (string.IsNullOrWhiteSpace(_currentPartners.Email) || !_currentPartners.Email.Contains("@"))
+            {
+                errors.AppendLine("Email должен содержать символ @.");
+            }
+
+            if (Rating.Text != "")
+            {
+                try
+                {
+                    _currentPartners.Rating = int.Parse(Rating.Text);
+                    if (int.Parse(Rating.Text) < 0) errors.AppendLine("Рейтинг не может быть отрицательным!");
+
+                                       
+                    if (Type.SelectedItem == null)
+                        MessageBox.Show("Выберите тип организации!");
+                }
+                catch (Exception)
+                {
+                    errors.AppendLine("Некорректное значение рейтинга!");
+                }
+            }
+            if (string.IsNullOrWhiteSpace(_currentPartners.CompanyName))
+                errors.AppendLine("Укажите наименование!");
+            if (string.IsNullOrWhiteSpace(_currentPartners.LegalAddress))
+                errors.AppendLine("Укажите Адрес!");
+
+            if ((_currentPartners.Rating == null))
+                errors.AppendLine("Введите рейтинг");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+
+            else
+            {
+                if (string.IsNullOrWhiteSpace(_currentPartners.CompanyName))
+                    errors.AppendLine("Укажите наименование!");
+                if (string.IsNullOrWhiteSpace(_currentPartners.LegalAddress))
+                    errors.AppendLine("Укажите Адрес!");
+                if ((_currentPartners.Rating == null))
+                    errors.AppendLine("Введите рейтинг");
+                if (errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString());
+                    return;
+                }
+                if (_currentPartners.PartnerID == 0)
+                    Entities1.GetContext().Partners.Add(_currentPartners);
+                try
+                {
+                    
+                    Entities1.GetContext().SaveChanges();
+                    MessageBox.Show("Данные успешно сохранены!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+    }
+}
+        
+    
+
---
+++ UP_02/Pages/PartnersPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UP_02.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PartnersPage.xaml
+    /// </summary>
+    public partial class PartnersPage : Page
+    {
+        private List<Partners> partners;
+
+        // Список данных о продажах
+        private List<PartnerProducts> salesData;
+
+
+        public PartnersPage()
+        {
+            InitializeComponent();
+            //partnersListView.ItemsSource = Entities1.GetContext().Partners.ToList();
+            partners = Entities1.GetContext().Partners.ToList();
+
+            salesData = Entities1.GetContext().PartnerProducts.ToList();
+
+            if (salesData == null)
+            {
+                MessageBox.Show("Данные о продажах отсутствуют!");
+                return;
+            }
+
+            // Расчет скидок
+            var discounts = DiscountCalculator.CalculateDiscounts(salesData);
+
+            // Привязка скидок к данным о партнерах
+            foreach (var partner in partners)
+            {
+                if (discounts.ContainsKey(partner.PartnerID))
+                {
+                    partner.Discount = discounts[partner.PartnerID];
+                }
+            }
+
+            // Привязка данных к ListView
+            partnersListView.ItemsSource = partners;
+
+
+
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+
+            {
+                Entities1.GetContext().ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+                partnersListView.ItemsSource = Entities1.GetContext().Partners.ToList();
+
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddPage(null));
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddPage((sender as Button).DataContext as Partners));
+        }
+
+        private void partnersListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is ListBox listBox)
+            {
+
+                var selectedItem = listBox.SelectedItem as Partners;
+
+
+                if (selectedItem != null)
+                {
+                    NavigationService.Navigate(new AddPage(selectedItem));
+                }
+            }
+        }
+
+        private void HistoryBTN_Click(object sender, RoutedEventArgs e)
+        {
+
+            var selectedItem = partnersListView.SelectedItem as Partners;
+            if (selectedItem != null)
+            {
+                NavigationService.Navigate(new HistoryPage(selectedItem));
+                 
+            }
+            else
+            {
+                MessageBox.Show("Выберите партнера");
+            }
+
+        }
+
+    }
+}
